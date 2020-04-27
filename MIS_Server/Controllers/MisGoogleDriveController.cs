8f28bc33da08ef65b90a8dc0b0c2ef49154d43c0
simonkuo0420@gmail.com <simonkuo0420@gmail.com>
2020-04-27 23:55:04 +0800

    Google Drive API: Uploading, Viewing, Downloading & Deleting Files

---
+++ MIS_Server/Controllers/MisGoogleDriveController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MIS_Server.Models;
+
+namespace MIS_Server.Controllers
+{
+    public class MisGoogleDriveController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetGoogleDriveFiles()
+        {
+            return View(GoogleDriveFilesRepository.GetDriveFiles());
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFile(GoogleDriveFiles file)
+        {
+            GoogleDriveFilesRepository.DeleteFile(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            GoogleDriveFilesRepository.FileUpload(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        public void DownloadFile(string id)
+        {
+            string FilePath = GoogleDriveFilesRepository.DownloadGoogleFile(id);
+
+
+            Response.ContentType = "application/zip";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(FilePath));
+            Response.WriteFile(System.Web.HttpContext.Current.Server.MapPath("~/GoogleDriveFiles/" + Path.GetFileName(FilePath)));
+            Response.End();
+            Response.Flush();
+        }
+    }
+}
