6874dadfee1a8d1f0f345379d250bb30250351b4
yckoinchu <eugene@g.chu.edu.tw>
2020-04-27 22:59:10 +0800

    first commit

---
+++ MIS_Server/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS_Server.Startup))]
+namespace MIS_Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
