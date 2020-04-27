74d1c33e45858946b158e19ea6c3ef107d6f737b
yckoinchu <eugene@g.chu.edu.tw>
2020-04-27 23:26:47 +0800

    push

---
+++ MIS_Server/Controllers/MisToolsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MIS_Server.Controllers
+{
+    public class MisToolsController : Controller
+    {
+        // GET: MisTools
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult upload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = System.IO.Path.GetFileName(file.FileName);
+                    var path = System.IO.Path.Combine(Server.MapPath("~/KoData/uploads"), fileName);
+                    file.SaveAs(path);
+                }
+
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
